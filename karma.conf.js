b2ee3762a2b8afd93b0258f112ba6321ed9a6d96
Nightapes <svbeisch@gmail.com>
2016-05-11 20:13:02 +0200

    Updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -68,7 +68,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: process.env.TRAVIS ? ['Chrome'] : ['Chrome'],
+        browsers: process.env.TRAVIS ? ['Firefox'] : [ 'Chrome'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
