344d644ffb5d75d41bc9949c710763596857b19a
Nightapes <svbeisch@gmail.com>
2016-05-17 19:17:49 +0200

    (feat) added tests

--- tests/password-validators.spec.ts
+++ tests/password-validators.spec.ts
@@ -49,5 +49,120 @@ export function main() {
                 expect(validated).toEqual({ 'whitespaceRule': true });
             });
         });
+
+        describe('allowedCharacterRule', () => {
+
+            it('should work for empty control', () => {
+                let control: Control = new Control('');
+                let allowedCharacter: string[] = ['a', 'b', 'c'];
+                let validated = PasswordValidators.allowedCharacterRule(allowedCharacter)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for valid password', () => {
+                let control: Control = new Control('aaabbbccc');
+                let allowedCharacter: string[] = ['a', 'b', 'c'];
+                let validated = PasswordValidators.allowedCharacterRule(allowedCharacter)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for invalid password with one wrong char', () => {
+                let control: Control = new Control('aaabbbcccd');
+                let allowedCharacter: string[] = ['a', 'b', 'c'];
+                let validated = PasswordValidators.allowedCharacterRule(allowedCharacter)(control);
+                expect(validated).toEqual({ 'allowedCharacterRule': ['d'] });
+            });
+
+            it('should work for invalid password with multiple wrong char', () => {
+                let control: Control = new Control('aaafb bbccc) d!');
+                let allowedCharacter: string[] = ['a', 'b', 'c'];
+                let validated = PasswordValidators.allowedCharacterRule(allowedCharacter)(control);
+                expect(validated).toEqual({ 'allowedCharacterRule': ['f', ' ', ')', 'd', '!'] });
+            });
+
+        });
+
+        describe('alphabeticalCharacterRule', () => {
+
+            it('should work for empty control', () => {
+                let control: Control = new Control('');
+                let validated = PasswordValidators.alphabeticalCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for valid password', () => {
+                let control: Control = new Control('aAa345b!bdDb"c cc76');
+                let validated = PasswordValidators.alphabeticalCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+
+            it('should work for invalid password', () => {
+                let control: Control = new Control('1234Abc!');
+                let validated = PasswordValidators.alphabeticalCharacterRule(5)(control);
+                expect(validated).toEqual({ 'alphabeticalCharacterRule': true });
+            });
+
+        });
+
+        describe('digitCharacterRule', () => {
+
+            it('should work for empty control', () => {
+                let control: Control = new Control('');
+                let validated = PasswordValidators.digitCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for valid password', () => {
+                let control: Control = new Control('aAa345b!bdDb"c cc76');
+                let validated = PasswordValidators.digitCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+
+            it('should work for invalid password', () => {
+                let control: Control = new Control('1234Abc!');
+                let validated = PasswordValidators.digitCharacterRule(5)(control);
+                expect(validated).toEqual({ 'digitCharacterRule': true });
+            });
+
+        });
+
+        describe('lowercaseCharacterRule', () => {
+
+            it('should work for empty control', () => {
+                let control: Control = new Control('');
+                let validated = PasswordValidators.lowercaseCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for valid password', () => {
+                let control: Control = new Control('abcdeAACDEF!231');
+                let validated = PasswordValidators.lowercaseCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+
+            it('should work for invalid password', () => {
+                let control: Control = new Control('1234abcADc!');
+                let validated = PasswordValidators.lowercaseCharacterRule(5)(control);
+                expect(validated).toEqual({ 'lowercaseCharacterRule': true });
+            });
+
+        });
+
+        describe('uppercaseCharacterRule', () => {
+
+            it('should work for empty control', () => {
+                let control: Control = new Control('');
+                let validated = PasswordValidators.uppercaseCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+
+            it('should work for valid password', () => {
+                let control: Control = new Control('abcdeAACDEF!231');
+                let validated = PasswordValidators.uppercaseCharacterRule(5)(control);
+                expect(validated).toBeUndefined();
+            });
+            it('should work for invalid password', () => {
+                let control: Control = new Control('1234abcAbc!');
+                let validated = PasswordValidators.uppercaseCharacterRule(5)(control);
+                expect(validated).toEqual({ 'uppercaseCharacterRule': true });
+            });
+
+        });
+
     });
 }
