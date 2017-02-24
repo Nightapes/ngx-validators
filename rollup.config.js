1cae7010560751a31037e2f3dec0591856e6f15f
Sebastian Beisch <sebastian.beisch@elektrobit.com>
2017-02-24 12:00:32 +0100

    build(rollup): add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+export default {
+    entry: './index.js',
+    dest: './bundles/ng2-validators.umd.js',
+    sourceMap: false,
+    format: 'umd',
+    onwarn: function (warning) {
+        // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+        // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+            return;
+        }
+        console.error(warning.message);
+    },
+    moduleName: 'ng2.validators',
+    globals: {
+        'google-libphonenumber': 'google.libphonenumber',
+        '@angular/core': 'ng.core',
+        '@angular/forms': 'ng.forms',
+        'rxjs/Observable': 'Rx',
+        'rxjs/ReplaySubject': 'Rx',
+        'rxjs/add/operator/map': 'Rx.Observable.prototype',
+        'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+        'rxjs/add/observable/fromEvent': 'Rx.Observable',
+        'rxjs/add/observable/of': 'Rx.Observable'
+    }
+}
