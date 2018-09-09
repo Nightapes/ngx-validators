3b75ca529f6d58abdec1cc1c07257835352ec00e
Nightapes <svbeisch@gmail.com>
2018-09-09 18:10:43 +0200

    docs(gh-page): add example for mismatch password

--- examples/src/app/guide/guide.module.ts
+++ examples/src/app/guide/guide.module.ts
@@ -2,14 +2,26 @@ import { MatCardModule, MatListModule } from '@angular/material';
 import { GuideComponent } from './guide.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: GuideComponent,
+  },
+  {
+    path: 'guide',
+    component: GuideComponent,
+  }
+];
+
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     MatListModule,
-    RouterModule
+    RouterModule.forChild(routes)
   ],
   declarations: [
     GuideComponent
