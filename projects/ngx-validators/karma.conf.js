9ab06fcf85e697f33d4a281e0c159b0121399f7a
Sebastian <svbeisch@gmail.com>
2021-11-12 22:43:12 +0100

    chore(ci): update karma

--- projects/ngx-validators/karma.conf.js
+++ projects/ngx-validators/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require("puppeteer").executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -34,7 +35,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["ChromeWithoutSecurity"],
+    customLaunchers: {
+      ChromeWithoutSecurity: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-setuid-sandbox"],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
